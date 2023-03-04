56ffbe2a693b086b3fd91c70724458b21bbeab5b
Mike Hummel <mh@mhus.de>
2023-03-05 00:48:51 +0100

    wip: install docker is working

--- terraform/hetzner_compose_server/install_docker.sh
+++ terraform/hetzner_compose_server/install_docker.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-apt-get remove docker docker-engine docker.io
-apt-get update
-apt-get install apt-transport-https ca-certificates curl software-properties-common
+apt update
+apt install -y apt-transport-https ca-certificates curl software-properties-common git
+apt install -y apparmor
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install docker-ce
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt update
+apt install -y docker-ce docker-compose-plugin
 docker run hello-world
--- terraform/hetzner_compose_server/main.tf
+++ terraform/hetzner_compose_server/main.tf
@@ -26,19 +26,40 @@ resource "null_resource" "wait_for_ssh" {
   depends_on = [hcloud_server.debian11]
 
   provisioner "local-exec" {
-    command = "while ! nc -z ${hcloud_server.debian11.ipv4_address} 22; do sleep 1; done"
+    command = "while ! nc -zG 2 ${hcloud_server.debian11.ipv4_address} 22; do echo \"Wait for ssh port\";sleep 10; done"
   }
 }
 
 resource "null_resource" "install_docker" {
   depends_on = [null_resource.wait_for_ssh]
+
   provisioner "remote-exec" {
     connection {
         host     = hcloud_server.debian11.ipv4_address
         user     = "root"
         private_key = tls_private_key.access.private_key_pem
+        timeout = "30m"
     }
     script = "install_docker.sh"
+  }
+}
+
+resource "null_resource" "run_coolweb" {
+  depends_on = [null_resource.install_docker]
+
+  provisioner "remote-exec" {
+    connection {
+        host     = hcloud_server.debian11.ipv4_address
+        user     = "root"
+        private_key = tls_private_key.access.private_key_pem
+        timeout = "30m"
+    }
+    inline = [
+    "git clone https://github.com/mhus/coolweb.git",
+    "cd coolweb",
+    "docker compose build",
+    "docker compose start"
+    ]
   }
 }
 
